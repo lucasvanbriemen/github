479f1a7220ea10863ef353d0ccfa3f2a55f6ada3
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-09-06 16:21:41 +0200

    dashboard

--- resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-{{ __("Hello world") }}
-
-{{ truncate_text("This is a long text that needs to be truncated", 20) }}
-
-@php
-$token = config('services.github.access_token');
-var_dump($token);
-
-@endphp
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\IsLoggedIn;
+use App\Http\Controllers\DashboardController;
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get("/", [DashboardController::class, "index"])->middleware(IsLoggedIn::class)->name("dashboard");
