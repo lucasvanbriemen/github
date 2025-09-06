2a441c89a4bcfecb86d8408448376f047c2d6ad4
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-09-06 18:47:35 +0200

    Organizations

---
+++ app/Models/Organization.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Organization extends Model
+{
+  protected $keyType = "string";
+  public $incrementing = false;
+
+  public static function booted()
+  {
+    static::creating(function ($organization) {
+      $organization->id = (string) Str::uuid();
+    });
+  }
+}
---
+++ database/migrations/2025_09_06_154428_create_organizations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create("organizations", function (Blueprint $table) {
+            $table->uuid("id")->primary();
+            $table->timestamps();
+            $table->string("organization_id");
+
+            $table->string("name");
+            $table->longText("description");
+            $table->string("avatar_url");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists("organizations");
+    }
+};
