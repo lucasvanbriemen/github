83b4663c8941fc102410fbb9ae2bda0285e579ef
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-09-06 12:42:11 +0200

    clean up

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import fg from 'fast-glob';
+import laravel from 'laravel-vite-plugin';
+
+const files = fg.sync([
+    'resources/js/**/*.js',
+    'resources/scss/**/*.scss',
+]);
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: files,
+            refresh: true,
+        }),
+    ],
+});
