eb7891722816d132090d9115166583ffbf8a9dff
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-09-08 22:08:37 +0200

    render Repos

--- resources/views/organization/show.blade.php
+++ resources/views/organization/show.blade.php
@@ -3,4 +3,11 @@
     <img class="logo" src="{{ $organization->avatar_url }}" alt="{{ $organization->name }}">
     <h1>{{ $organization->name }}</h1>
   </x-slot>
+
+  <div class="repositories">
+    @foreach ($repositories as $repository)
+      <div class="repository-card">
+        <h2>{{ $repository->name }}</h2>
+      </div>
+    @endforeach
 </x-app-layout>
