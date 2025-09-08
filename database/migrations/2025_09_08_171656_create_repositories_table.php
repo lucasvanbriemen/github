be2be583291afa60ab3f5e3c75b8f4650e52cf63
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-09-08 19:23:12 +0200

    add repo tab;e

---
+++ database/migrations/2025_09_08_171656_create_repositories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('repositories', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->timestamps();
+
+            // personal reposos dont have organization id
+            $table->uuid('organization_id')->nullable();
+
+            $table->string('name');
+            $table->string('full_name')->unique();
+            $table->boolean('private');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('repositories');
+    }
+};
