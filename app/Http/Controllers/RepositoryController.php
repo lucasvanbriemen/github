7e22939c5c99c00142362b684585c30afda28999
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-09-08 20:55:54 +0200

    ordering

--- app/Http/Controllers/RepositoryController.php
+++ app/Http/Controllers/RepositoryController.php
@@ -19,8 +19,18 @@ class RepositoryController extends Controller
   {
     $apiRepos = ApiHelper::githubApi("/user/repos");
     foreach ($apiRepos as $apiRepo) {
+      $organizationId = null;
+
+      // If the repo belongs to an organization you own, find its ID
+      if ($apiRepo->owner->type === 'Organization') {
+        $org = Organization::where('name', $apiRepo->owner->login)->first();
+        if ($org) {
+          $organizationId = $org->id;
+        }
+      }
+
       Repository::updateOrCreate(
-        ["organization_id" => null, "name" => $apiRepo->name],
+        ["organization_id" => $organizationId, "name" => $apiRepo->name],
         [
           "full_name" => $apiRepo->full_name,
           "private" => $apiRepo->private,
@@ -38,7 +48,7 @@ class RepositoryController extends Controller
       $apiRepos = ApiHelper::githubApi("/orgs/{$organization->name}/repos");
       foreach ($apiRepos as $apiRepo) {
         Repository::updateOrCreate(
-          ["organization_id" => $organization->name, "name" => $apiRepo->name],
+          ["organization_id" => $organization->id, "name" => $apiRepo->name],
           [
             "full_name" => $apiRepo->full_name,
             "private" => $apiRepo->private,
