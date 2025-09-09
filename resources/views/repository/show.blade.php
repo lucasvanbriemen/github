f81b525f13b237ac85dee2870b294ac906c9d4cd
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-09-09 22:28:02 +0200

    list file

--- resources/views/repository/show.blade.php
+++ resources/views/repository/show.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout class="dashboard-page">
+<x-app-layout class="repository-page">
   <x-slot:header>
     @if ($organization)
       <img class="logo" src="{{ $organization->avatar_url }}" alt="{{ $organization->name }}">
@@ -6,9 +6,19 @@
     <h1>{{ $repository->full_name }}</h1>
   </x-slot>
 
+  <div class="navigation">
+    <a href="{{ route('organization.show', $organization->name) }}">Back to {{ $organization->login }}</a>
+  </div>
+
   @if (empty($filecontent))
     @include("repository.no_content")
   @else
-    {{ $filecontent[0]->name }}
+    <div class="file-list">
+      @foreach ($filecontent as $file)
+        <div class="file">
+          <h2 class="name">{{ $file->name }}</h2>
+        </div>
+      @endforeach
+    </div>
   @endif
 </x-app-layout>
