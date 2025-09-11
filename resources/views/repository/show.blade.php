24f0f5f2fe45bcac35f3b2a344e222669c12aeec
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-09-11 16:20:25 +0200

    extract file/folder to sepreate patioan

--- resources/views/repository/show.blade.php
+++ resources/views/repository/show.blade.php
@@ -15,20 +15,7 @@
   @if (empty($filecontent))
     @include("repository.no_content")
   @elseif (!$isFile)
-    <div class="file-list">
-      @foreach ($filecontent as $file)
-        <a class="file"href="{{ route('repository.show', [$organization->name, $repository->name, $file->path, 'isFile' => ($file->type === "file")]) }}">
-
-          @if ($file->type === 'dir')
-            {!! svg('folder') !!}
-          @elseif ($file->type === 'file')
-            {!! svg('file') !!}
-          @endif
-
-          <h2 class="name">{{ $file->name }}</h2>
-        </a>
-      @endforeach
-    </div>
+    @include("repository.file_navigation")
   @else
     <div class="file-list"><pre>{!! $filecontent !!}</pre></div>
   @endif
