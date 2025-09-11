6699ff96cbd3bf2c83325438adae9ca048a5a68f
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-09-11 17:50:46 +0200

    use object

--- resources/js/theme.js
+++ resources/js/theme.js
@@ -1,25 +1,23 @@
-export default (() => {
-  const themeUrl = "https://components.lucasvanbriemen.nl/api/colors?theme=THEME_NAME";
-  const selectedTheme = "auto";
+export default {
+  themeUrl: "https://components.lucasvanbriemen.nl/api/colors?theme=THEME_NAME",
+  selectedTheme: "auto",
 
-  const getTheme = () => {
-    if (selectedTheme === "auto") {
+  getTheme() {
+    if (this.selectedTheme === "auto") {
       const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
       return darkModeMediaQuery.matches ? "dark" : "light";
     }
 
-    return selectedTheme;
-  };
+    return this.selectedTheme;
+  },
 
-  const applyTheme = async () => {
-    document.documentElement.setAttribute("data-theme", getTheme());
-    const url = themeUrl.replace("THEME_NAME", getTheme());
-    const colors = await window.api.get(url);
+  async applyTheme() {
+    document.documentElement.setAttribute("data-theme", this.getTheme());
+    const url = this.themeUrl.replace("THEME_NAME", this.getTheme());
+    const colors = await api.get(url);
 
     colors.forEach(color => {
       document.documentElement.style.setProperty(`--${color.name}`, color.value);
     });
-  };
-
-  return { getTheme, applyTheme };
-})();
+  },
+};
