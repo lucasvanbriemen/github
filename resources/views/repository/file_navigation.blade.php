946f058b1b9d50385f2b6332a012ac369f1ecb27
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-09-11 16:20:45 +0200

    \

--- resources/views/repository/file_navigation.blade.php
+++ resources/views/repository/file_navigation.blade.php
@@ -1,11 +1,11 @@
 <div class="file-list">
   @foreach ($filecontent as $file)
-    <a class="file"href="{{ route('repository.show', [$organization->name, $repository->name, $file->path, 'isFile' => ($file->type === "file")]) }}">
+    <a class="file"href="{{ route("repository.show", [$organization->name, $repository->name, $file->path, "isFile" => ($file->type === "file")]) }}">
 
-      @if ($file->type === 'dir')
-        {!! svg('folder') !!}
-      @elseif ($file->type === 'file')
-        {!! svg('file') !!}
+      @if ($file->type === "dir")
+        {!! svg("folder") !!}
+      @elseif ($file->type === "file")
+        {!! svg("file") !!}
       @endif
 
       <h2 class="name">{{ $file->name }}</h2>
