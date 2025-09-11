5213f56c84024a5b29de27befb55165abb67bba0
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-09-11 17:19:47 +0200

    allow for naviationg folders

--- resources/js/repository.js
+++ resources/js/repository.js
@@ -4,4 +4,17 @@ export default (() => {
     console.log(data);
     document.querySelector(".file-list").innerHTML = data;
   });
+
+  const fileList = document.querySelector(".file-list");
+  fileList.addEventListener("click", (event) => {
+    if (event.target.classList.contains("file")) {
+      event.preventDefault();
+      const url = new URL(event.target.href);
+      history.pushState({}, "", url);
+      api.get(window.location.origin + `/api${url.pathname}/`).then((data) => {
+        console.log(data);
+        document.querySelector(".file-list").innerHTML = data;
+      });
+    }
+  });
 });
