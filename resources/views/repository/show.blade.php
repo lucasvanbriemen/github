fae1e66444362813f6da35af795a8393ec5d7f1d
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-09-11 15:34:11 +0200

    add header links

--- resources/views/repository/show.blade.php
+++ resources/views/repository/show.blade.php
@@ -1,9 +1,11 @@
 <x-app-layout class="repository-page">
   <x-slot:header>
     @if ($organization)
-      <img class="logo" src="{{ $organization->avatar_url }}" alt="{{ $organization->name }}">
+      <a href="{{ route('organization.show', $organization->name) }}">  
+        <img class="logo" src="{{ $organization->avatar_url }}" alt="{{ $organization->name }}">
+      </a>
     @endif
-    <h1>{{ $repository->full_name }}</h1>
+    <a href="{{ route('repository.show', [$organization->name, $repository->name]) }}">{{ $repository->full_name }}</a>
   </x-slot>
 
   <div class="navigation">
