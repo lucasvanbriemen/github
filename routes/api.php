e170e4ddb7e6d234441bce2eb9c719f9bcde7189
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-09-11 17:20:48 +0200

    allow for more than 1 deep searhing

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,5 @@ use App\Http\Middleware\IsLoggedIn;
 
 Route::patch("/organizations", [OrganizationController::class, "updateOrganizations"])->middleware(IsLoggedIn::class)->name("organizations.update");
 Route::patch("/organizations/repositories", [RepositoryController::class, "updateRepositories"])->middleware(IsLoggedIn::class)->name("organizations.repositories.update");
-Route::get("/organization/{organization}/{repository}/tree/{file_path?}", [RepositoryController::class, "show_file_tree"])->middleware(IsLoggedIn::class)->name("organizations.repositories.show");
+Route::get("/organization/{organization}/{repository}/tree/{file_path?}", [RepositoryController::class, "show_file_tree"])->middleware(IsLoggedIn::class)->name("organizations.repositories.show")
+->where('file_path', '.*');
