92e4faa3dbff0bb3db9c98e6dd8adb82b707ec37
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-09-11 17:51:42 +0200

    dry

--- resources/js/dashboard.js
+++ resources/js/dashboard.js
@@ -3,19 +3,19 @@ export default {
     const updateOrganizations = document.querySelector(".update-organizations");
     updateOrganizations.addEventListener("click", () => {
       const url = window.location.origin + "/api/organizations";
-
-      api.patch(url).then((data) => {
-        window.location.reload();
-      });
+      this.sendRequest(url);
     });
 
     const updateRepositories = document.querySelector(".update-repositories");
     updateRepositories.addEventListener("click", () => {
       const url = window.location.origin + "/api/organizations/repositories";
-
-      api.patch(url).then((data) => {
-        window.location.reload();
-      });
+      this.sendRequest(url);
     });
   },
+
+  sendRequest(url) {
+    return api.patch(url).then((data) => {
+      window.location.reload();
+    });
+  }
 };
