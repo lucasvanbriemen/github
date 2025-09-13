49d2f4ce3c501e66af64f11bc325270153b7da28
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-09-13 17:17:28 +0200

    improve sidebar styling

--- resources/views/repository/sidebar.blade.php
+++ resources/views/repository/sidebar.blade.php
@@ -1,6 +1,14 @@
 <div class="sidebar">
-  <a href="{{ route("repository.issues.show", [$organization->name, $repository->name]) }}" class="sidebar-item">Show issues <span>{{ $repository->openIssues->count()  }}</span></a>
-  <a href="{{ route("organization.show", [$organization->name, $repository->name]) }}" class="sidebar-item">Show PR <span>{{ $repository->pr_count }}</span></a>
-  <a href="{{ route("organization.show", $organization->name) }}" class="sidebar-item">Show org</a>
-  <a href="{{ route("organization.show", $organization->name) }}" class="sidebar-item">Show org</a>
+  <a href="{{ route("repository.issues.show", [$organization->name, $repository->name]) }}" class="sidebar-item {{ request()->routeIs('repository.issues.*') ? 'active' : '' }}">
+    Show issues <span class="count">{{ $repository->openIssues->count() }}</span>
+  </a>
+  <a href="{{ route("organization.show", [$organization->name, $repository->name]) }}" class="sidebar-item {{ request()->routeIs('repository.pr.*') ? 'active' : '' }}">
+    Show PR <span class="count">{{ $repository->pr_count }}</span>
+  </a>
+  <a href="{{ route("organization.show", $organization->name) }}" class="sidebar-item">
+    Show org
+  </a>
+  <a href="{{ route("organization.show", $organization->name) }}" class="sidebar-item">
+    Show org
+  </a>
 </div>
