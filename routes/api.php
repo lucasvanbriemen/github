e844109e612401e88ab4372ad7c0a1ae5628c169
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-09-18 21:04:15 +0200

    add ;

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OrganizationController;
 use App\Http\Controllers\RepositoryController;
-use App\Http\Controllers\IncomingWebookController
+use App\Http\Controllers\IncomingWebookController;
 use App\Http\Middleware\IsLoggedIn;
 
 Route::patch("/organizations", [OrganizationController::class, "updateOrganizations"])->middleware(IsLoggedIn::class)->name("organizations.update");
