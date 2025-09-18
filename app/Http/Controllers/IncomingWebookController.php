6f51827cb9f14b39f1543a0aa54b911d54fc742d
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-09-18 21:10:29 +0200

    Remove comments.

--- app/Http/Controllers/IncomingWebookController.php
+++ app/Http/Controllers/IncomingWebookController.php
@@ -9,11 +9,9 @@ class IncomingWebookController extends Controller
     //
     public function index(Request $request)
     {
-        // Get the headers and payload
         $headers = $request->headers->all();
         $payload = $request->all();
 
-        // Get the event type from headers
         $eventType = $headers['x-github-event'][0] ?? 'unknown';
 
         return response()->json(["message" => "received", "event" => $eventType], 200);
