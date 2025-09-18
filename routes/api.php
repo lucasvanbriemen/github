7d06bf9b35d090fc0bbcb2a5e303a019a91c5cbd
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-09-18 20:46:12 +0200

    test

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,6 @@ Route::get("/organization/{organization}/{repository}/tree/{file_path?}", [Repos
   ->name("api.repositories.show")
   ->where('file_path', '.*');
 
-
 Route::get("incoming_hook", function (Request $request) {
     return response()->json(["message" => "received"], 200);
 })->name("api.webhook.get");
