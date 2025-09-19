fe7a84c783d4fe69b5cea69975a37b387a868d6d
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-09-19 20:03:52 +0200

    Use double quotes.

--- resources/views/dashboard/show.blade.php
+++ resources/views/dashboard/show.blade.php
@@ -3,12 +3,12 @@
     <h1>{{ __("Dashboard") }}</h1>
   </x-slot>
 
-  <img src='{{ gravatar() }}' alt='{{ currentUser()->name }}' class='avatar'>
+  <img src="{{ gravatar() }}" alt="{{ currentUser()->name }}" class="avatar">
 
   <div class="repositories">
     @foreach ($repositories as $repository)
-      @php $owner = explode('/', $repository->full_name)[0] ?? 'user'; @endphp
-      <a class="repository-card card" href="{{ route('repository.show', [$owner, $repository->name]) }}">
+      @php $owner = explode("/", $repository->full_name)[0] ?? "user"; @endphp
+      <a class="repository-card card" href="{{ route("repository.show", [$owner, $repository->name]) }}">
         <h2 class="name">{{ $repository->full_name }}</h2>
       </a>
     @endforeach
