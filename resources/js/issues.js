25aa035ff57ce9c166b0a5ae6ecb495ef2431dd1
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-09-20 16:15:21 +0200

    Send filtered data to API.

--- resources/js/issues.js
+++ resources/js/issues.js
@@ -1,6 +1,13 @@
 export default {
   init() {
-    const url = window.location.origin + "/api/organization/" + window.organizationName + "/" + window.repositoryName + "/issues";
+    this.updateIssues();
+  },
+
+  updateIssues() {
+    const state = document.querySelector(".filters select[name=state]").value;
+    const assignee = document.querySelector(".filters select[name=assignee]").value;
+
+    const url = window.location.origin + "/api/organization/" + window.organizationName + "/" + window.repositoryName + "/issues?state=" + state + "&assignee=" + assignee;
     api.get(url).then((data) => {
       const issues = document.querySelector(".issues-wrapper");
       issues.innerHTML = data
