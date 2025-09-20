6825beacaf8c02c3676841bd71cc0e7df18f5ba4
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-09-20 14:51:35 +0200

    Use components for search select.

--- resources/views/repository/issue/filter.blade.php
+++ resources/views/repository/issue/filter.blade.php
@@ -1,24 +1,7 @@
 <div class="filters card">
-  <div class="search-select-wrapper">
-    <select class="search-select">
-      <option value="open">Open</option>
-      <option value="closed">Closed</option>
-      <option value="all">All</option>
-    </select>
-
-    <div class="select-ui-wrapper">
-      <input class="search-input" type="text" placeholder="Search issues">
-      <div class="option-wrapper">
-        <div class="option-item" data-value="open">
-          <span class="main-text">Open</span>
-        </div>
-        <div class="option-item" data-value="closed">
-          <span class="main-text">Closed</span>
-        </div>
-        <div class="option-item" data-value="all">
-          <span class="main-text">All</span>
-        </div>
-      </div>
-    </div>
-  </div>
+  <x-search-select
+    :options="['open' => 'Open', 'closed' => 'Closed', 'all' => 'All']"
+    placeholder="Search issues"
+    name="issue-filter"
+    selected="open" />
 </div>
