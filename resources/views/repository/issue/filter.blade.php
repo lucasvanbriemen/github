ad5ba846139ac302c99f8c8e62198a76d3aa2411
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-09-20 14:49:11 +0200

    Search and select issue state.

--- resources/views/repository/issue/filter.blade.php
+++ resources/views/repository/issue/filter.blade.php
@@ -1,3 +1,24 @@
-<div class="issue-card card">
- 
+<div class="filters card">
+  <div class="search-select-wrapper">
+    <select class="search-select">
+      <option value="open">Open</option>
+      <option value="closed">Closed</option>
+      <option value="all">All</option>
+    </select>
+
+    <div class="select-ui-wrapper">
+      <input class="search-input" type="text" placeholder="Search issues">
+      <div class="option-wrapper">
+        <div class="option-item" data-value="open">
+          <span class="main-text">Open</span>
+        </div>
+        <div class="option-item" data-value="closed">
+          <span class="main-text">Closed</span>
+        </div>
+        <div class="option-item" data-value="all">
+          <span class="main-text">All</span>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
