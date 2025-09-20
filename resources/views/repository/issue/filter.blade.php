f827ac12a25686df3c983f1af9a7e546c2a138e8
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-09-20 15:24:34 +0200

    Add assignee select.

--- resources/views/repository/issue/filter.blade.php
+++ resources/views/repository/issue/filter.blade.php
@@ -2,6 +2,18 @@
   <x-search-select
     :options="['open' => 'Open', 'closed' => 'Closed', 'all' => 'All']"
     placeholder="Search issues"
-    name="issue-filter"
+    name="state"
     selected="open" />
+
+    @php
+      $options = [];
+      foreach ($repository->users as $user) {
+        $options[$user['id']] = $user['name'];
+      }
+    @endphp
+
+    <x-search-select
+    :options="$options"
+    placeholder="Search assignee"
+    name="assignee" />
 </div>
