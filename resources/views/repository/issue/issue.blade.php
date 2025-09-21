9922cb01a1d5d33108ee4008464771ac0664f755
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-09-21 17:22:03 +0200

    Render labels and fix user list overflowing.

--- resources/views/repository/issue/issue.blade.php
+++ resources/views/repository/issue/issue.blade.php
@@ -42,5 +42,24 @@
           <span>{{ $assignee->name }}</span>
         </div>
       @endforeach
+    </div>
+
+    <div class="issue-detail labels">
+      <h3>Labels</h3>
+      @foreach (json_decode($issue->labels, true) as $label)
+        @php
+          $hex = ltrim($label['color'], '#');
+          $r = hexdec(substr($hex, 0, 2));
+          $g = hexdec(substr($hex, 2, 2));
+          $b = hexdec(substr($hex, 4, 2));
+          $luminance = (0.299*$r + 0.587*$g + 0.114*$b) / 255;
+          $textColor = $luminance > 0.5 ? '#000000' : '#FFFFFF';
+        @endphp
+
+        <span class="label" style="background-color: #{{ $label['color'] }}; color: {{ $textColor }};">
+          {{ $label['name'] }}
+        </span>
+      @endforeach
+    </div>
   </div>
 </x-app-layout>
