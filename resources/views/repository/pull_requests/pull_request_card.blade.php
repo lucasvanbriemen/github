144ccf5339798278ba39ff5d96a4923fc2e298af
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-09-25 18:13:35 +0200

    Remove IDs.

--- resources/views/repository/pull_requests/pull_request_card.blade.php
+++ resources/views/repository/pull_requests/pull_request_card.blade.php
@@ -4,7 +4,7 @@
     <h3 class='issue-title'>
       {{ $pullRequest->title }}
     </h3>
-    <span class='opened-by'>Opened by {{ $pullRequest->opened_by_id }} {{ $pullRequest->openedBy->name }} <img src="{{ $pullRequest->openedBy->avatar_url }}" alt="{{ $pullRequest->openedBy->name }}">{{ $pullRequest->created_at->diffForHumans() }}</span>
+    <span class='opened-by'>Opened by {{ $pullRequest->openedBy->name }} <img src="{{ $pullRequest->openedBy->avatar_url }}" alt="{{ $pullRequest->openedBy->name }}">{{ $pullRequest->created_at->diffForHumans() }}</span>
   </div>
 </div>
 </a>
