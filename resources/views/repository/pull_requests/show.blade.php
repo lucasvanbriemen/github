beeeca50bbafb630fb387c36865c186539c30f7e
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-09-25 20:38:18 +0200

    Render comments.

--- resources/views/repository/pull_requests/show.blade.php
+++ resources/views/repository/pull_requests/show.blade.php
@@ -21,6 +21,24 @@
       </div>
       <div class='markdown-body'><x-markdown theme="github-dark">{!! $pullRequest->body !!}</x-markdown></div>
     </div>
+
+    @foreach ($pullRequest->comments as $comment)
+      <div class="issue-comment {{ $comment->resolved ? 'resolved' : '' }}" data-comment="{{ $comment->github_id }}">
+        <div class="comment-header">
+          <span class="author"><img src="{{ $comment->author?->avatar_url }}" alt="{{ $comment->author?->name }}"> {{ $comment->author?->name }}</span>
+          <span class="created-at">{{ $comment->created_at->diffForHumans() }}</span>
+        </div>
+        <div class='markdown-body'>
+          <x-markdown theme="github-dark">{!! $comment->body !!}</x-markdown>
+          
+          @if(!$comment->resolved)
+            <button class="button-primary resolve-comment" data-url="{{ route('api.repositories.issues.comment.resolve', [$organization->name, $repository->name, $pullRequest->number, $comment->github_id]) }}" data-comment="{{ $comment->github_id }}">Mark as resolved</button>
+          @else
+            <button class="button-primary unresolve-comment" data-url="{{ route('api.repositories.issues.comment.unresolve', [$organization->name, $repository->name, $pullRequest->number, $comment->github_id]) }}" data-comment="{{ $comment->github_id }}">Mark as unresolved</button>
+          @endif
+        </div>
+      </div>
+    @endforeach
   </div>
 
   <div class="pull-request-details">
