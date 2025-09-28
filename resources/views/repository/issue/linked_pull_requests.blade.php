d8f5ab8d70d4cccc93d3c68cd2f9b0693fb912b3
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-09-28 18:37:19 +0200

    Render the correct SVG.

--- resources/views/repository/issue/linked_pull_requests.blade.php
+++ resources/views/repository/issue/linked_pull_requests.blade.php
@@ -1,7 +1,7 @@
 <h3>Linked Pull Requests</h3>
 @foreach ($pullRequests as $pullRequest)
   <div class="linked-pull-request pull-request-{{ $pullRequest->state }}">
-    <span class="icon">{!! svg('pull-request') !!}</span>
+    <span class="icon">{!! svg('pull_request') !!}</span>
     <a href="{{ route('repository.prs.show', [$organizationName, $repositoryName, $pullRequest->number]) }}">
       {{ $pullRequest->title }}
     </a>
