224a10fb61d535412394da548f8f718e532e83e9
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-10-04 23:36:06 +0200

    Style-related comments.

--- resources/views/emails/pull_request_reviewed.blade.php
+++ resources/views/emails/pull_request_reviewed.blade.php
@@ -8,7 +8,17 @@
     <x-markdown theme="github-dark">{!! $pullRequestReview->body !!}</x-markdown>
   </div>
 
-  @dump($pullRequestReview->relatedComments)
+  @foreach ($pullRequestReview->relatedComments as $comment)
+    <hr>
+
+    <div class='email-header'>
+      <img src="{{ $comment->author->avatar_url }}" alt="{{ $comment->author->name }}'s avatar" class='email-avatar'>
+      {{ $comment->author->name }} commented:
+    </div>
+    <div class='email-body'>
+      <x-markdown theme="github-dark">{!! $comment->body !!}</x-markdown>
+    </div>
+  @endforeach
 
   @vite(["resources/scss/emails/pull_request_reviewed.scss", "resources/scss/shared/markdown.scss"])
 </x-email-layout>
