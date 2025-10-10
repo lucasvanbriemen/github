2c20915296c059bf423faaf17bed4dfcd8e05dec
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-10-10 19:53:10 +0200

    Remove unnecessary API functions.

--- app/Helpers/ApiHelper.php
+++ app/Helpers/ApiHelper.php
@@ -76,61 +76,6 @@ class ApiHelper
         $systemInfo->save();
     }
 
-    public static function githubApiPatch($route, array $data)
-    {
-        self::init();
-        self::updateSystemInfo(self::BASE_URL.$route);
-
-        $fullUrl = self::BASE_URL.$route;
-
-        $ch = curl_init($fullUrl);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PATCH');
-        $headers = self::formatHeaders();
-        $headers[] = 'Content-Type: application/json';
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-
-        $responseBody = curl_exec($ch);
-        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
-
-        if ($httpCode >= 200 && $httpCode < 300) {
-            return json_decode($responseBody);
-        }
-
-        return null;
-    }
-
-    public static function githubApiPut($route, array $data)
-    {
-        self::init();
-        self::updateSystemInfo(self::BASE_URL.$route);
-
-        $fullUrl = self::BASE_URL.$route;
-
-        $ch = curl_init($fullUrl);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
-        $headers = self::formatHeaders();
-        $headers[] = 'Content-Type: application/json';
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-        // Ensure empty arrays are encoded as objects for GitHub API compatibility
-        $jsonData = json_encode($data, JSON_FORCE_OBJECT);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-
-        $responseBody = curl_exec($ch);
-        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
-
-        if ($httpCode >= 200 && $httpCode < 300) {
-            return json_decode($responseBody);
-        }
-
-        return null;
-    }
-
     private static function formatHeaders()
     {
         $formatted = [];
