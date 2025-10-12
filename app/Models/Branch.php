ef1c5aad5fc59e2844f7f76bd36eff3f48d15707
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-10-12 12:11:52 +0200

    Use const for System.Object[].

--- app/Models/Branch.php
+++ app/Models/Branch.php
@@ -23,18 +23,15 @@ class Branch extends Model
 
     public function showNotice()
     {
-        // If its master or maon, we don't show the notice
         $show = true;
         if (in_array($this->name, self::MASTER_BRANCHES)) {
             $show = false;
         }
 
-        // If it has a pull request, we don't show the notice
         if ($this->hasPullRequest) {
             $show = false;
         }
 
-        // If its created more than 6 hours ago, we don't show the notice
         $sixHoursAgo = now()->subHours(self::NOTICE_CREATED_TIME_HOURS);
         if ($this->created_at < $sixHoursAgo) {
             $show = false;
