3598d317c380718597b9e4d255cdc16f669ba1e5
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-11-28 22:35:37 +0100

    Add disabled state.

--- resources/js/components/item/pr/FileNavigation.svelte
+++ resources/js/components/item/pr/FileNavigation.svelte
@@ -1,13 +1,14 @@
 <script>
-  let { files = [], selectedFileIndex = bindable(0), selectedFile = bindable(null) } = $props();
+  let { files = [], selectedFileIndex = $bindable(0), selectedFile = $bindable(null) } = $props();
 </script>
 
-<div class="file-nav-wrapper">
-  <button onclick={() => selectedFileIndex--} class="file-nav-button" class:disabled={selectedFileIndex === 0} type="button">Previous File</button>
+<div class="tab-navigation">
+  <button onclick={() => selectedFileIndex--} class="tab-button" disabled={selectedFileIndex === 0} type="button">Previous File</button>
   <span class="file-nav-info">File {selectedFileIndex + 1} of {files.length}: {selectedFile?.filename}</span>
-  <button onclick={() => selectedFileIndex++} class="file-nav-button" class:disabled={selectedFileIndex === files.length - 1} type="button">Next File</button>
+  <button onclick={() => selectedFileIndex++} class="tab-button" disabled={selectedFileIndex === files.length - 1} type="button">Next File</button>
 </div>
 
 <style lang="scss">
-  @import '../../../../scss/components/item/pr/filetab/navigation';
+  @import '../../../../scss/components/item/navigation';
+
 </style>
