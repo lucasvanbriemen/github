54157620a545bdcacb88cd51f5c9a2d65753c118
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-11-28 22:36:48 +0100

    Clean up.

--- resources/js/components/item/pr/FileNavigation.svelte
+++ resources/js/components/item/pr/FileNavigation.svelte
@@ -4,7 +4,6 @@
 
 <div class="tab-navigation">
   <button onclick={() => selectedFileIndex--} class="tab-button" disabled={selectedFileIndex === 0} type="button">Previous File</button>
-  <span class="file-nav-info">File {selectedFileIndex + 1} of {files.length}: {selectedFile?.filename}</span>
   <button onclick={() => selectedFileIndex++} class="tab-button" disabled={selectedFileIndex === files.length - 1} type="button">Next File</button>
 </div>
 
