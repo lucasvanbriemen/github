3a8aa44a3a22af0028e7775f6412e4ec08792f3c
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-12-06 17:09:59 +0100

    Cleanup.

--- resources/js/components/Markdown.svelte
+++ resources/js/components/Markdown.svelte
@@ -124,7 +124,7 @@
 
       {#if isEditing}
         <div class="markdown-shortcuts">
-          {#each Object.entries(shortcutMap) as [shortcut]}
+          {#each Object.entries(shortcutMap) as [key, shortcut]}
             <button class="markdown-shortcut button-primary-outline" onclick={() => insertShortcut(shortcut.key)}>{shortcut.title}</button>
           {/each}
         </div>
