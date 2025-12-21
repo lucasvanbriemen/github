9ff21ce9f1c52ab0fd08601ddceb44cb0a7fa9ca
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-12-21 11:17:28 +0100

    Render button.

--- resources/js/components/item/pr/MergePanel.svelte
+++ resources/js/components/item/pr/MergePanel.svelte
@@ -1,9 +1,26 @@
 <script>
   let { item, params } = $props();
+
+  let organization = params.organization;
+  let repository = params.repository;
+  let number = params.number;
+
+  function close() {
+    api.post(route(`organizations.repositories.pr.close`, { organization, repository, number }));
+    item.state = 'closed';
+  }
+
+  function merge() {
+    api.post(route(`organizations.repositories.pr.merge`, { organization, repository, number }));
+    item.state = 'merged';
+  }
 </script>
 
 <div class="merge-panel">
-  
+  {#if item.state === 'open'}
+    <button class="button-primary" onclick={() => close()}>Close Pull Request</button>
+    <button class="button-primary" onclick={() => merge()}>Merge Pull Request</button>
+  {/if}
 </div>
 
 <style lang="scss">
