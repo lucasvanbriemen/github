5ce0e26ca32ba61b6c96d9aed56feb6a72b9e08a
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-12-27 16:07:10 +0100

    Add repositories.

--- resources/js/components/Dashboard.svelte
+++ resources/js/components/Dashboard.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { organization, repository } from './stores';
   import { onMount } from 'svelte';
 
   let organizations = [];
@@ -9,9 +10,22 @@
 
 </script>
 
-{#each organizations as organization}
-  <span>{organization.name}</span><br />
-{/each}
+<main>
+  <div class="organizations">
+    {#each organizations as org}
+      <button class="organization" onclick={() => organization.set(org.name)}>
+        <img src="{org.avatar_url}" alt="{org.name} Avatar" width="50" height="50" />
+        <span class="org">{org.name}</span>
+
+        {#if $organization == org.name}
+          {#each org.repositories as repo}
+            <a href={`#/${org.name}/${repo.name}`} class="repo" onclick={() => repository.set(repo.name)}>{repo.name}</a>
+          {/each}
+        {/if}
+      </button>
+    {/each}
+  </div>
+</main>
 
 <style>
 </style>
