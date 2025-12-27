661b2985a105a350d1c1cc0ae1976ece74310bec
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-12-27 17:05:25 +0100

    Add comment.

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Notification extends Model
 {
+    public function comment()
+    {
+        return $this->belongsTo(BaseComment::class, 'related_id', 'id');
+    }
+
     protected $fillable = [
         'type',
         'related_id',
