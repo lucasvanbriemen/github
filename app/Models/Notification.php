a4a5ad870fa2e2fdec9d7ae75e53c93f3ac726b9
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-12-27 18:50:49 +0100

    Remove unnecessary changes.

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -6,11 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Notification extends Model
 {
-    public function comment()
-    {
-        return $this->belongsTo(BaseComment::class, 'related_id', 'id');
-    }
-
     protected $fillable = [
         'type',
         'related_id',
