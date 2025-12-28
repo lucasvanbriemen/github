9147ce3bb64ef3476110bd103506f20681ce57e1
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-12-28 13:42:14 +0100

    Use a tag.

--- resources/js/components/Notification.svelte
+++ resources/js/components/Notification.svelte
@@ -14,19 +14,29 @@
     }
   }
 
+  function getNotificationUrl(notification) {
+    if (notification.type === 'comment_mention') {
+      if (notification.comment.item.type === 'issue') {
+        return `#/issues/${notification.comment.item.number}`;
+      } else if (notification.comment.item.type === 'pull_request') {
+        return `#/prs/${notification.comment.item.number}`;
+      }
+    }
+  }
+
   function completeNotification(id) {
     console.log('Complete notification');
   }
 </script>
 
-<div class="notification">
+<a class="notification" href="{getNotificationUrl(notification)}" >
   <div>
     <h3 class="title">{getNotificationTitle(notification)}</h3>
     <p class="body">{getNotificationBody(notification)}</p>
   </div>
 
   <Icon name="approved" size="1.5rem" onclick={() => completeNotification(notification.id)} />
-</div>
+</a>
 
 <style>
   @import "../../scss/components/notification.scss";
