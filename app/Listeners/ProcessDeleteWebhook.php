4aae663f017cf74e85293a97416d941dd2d95596
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-01-17 23:16:00 +0100

    Delete branches.

---
+++ app/Listeners/ProcessDeleteWebhook.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\DeleteWebhookReceived;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use App\Models\Branch;
+
+class ProcessDeleteWebhook implements ShouldQueue
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(DeleteWebhookReceived $event): void
+    {
+        $payload = $event->payload;
+
+        if (isset($payload->ref_type) && $payload->ref_type === 'branch' && isset($payload->ref)) {
+            Branch::delete(
+                [
+                    'name' => $payload->ref,
+                    'repository_id' => $payload->repository->id
+                ]
+            );
+        }
+    }
+}
