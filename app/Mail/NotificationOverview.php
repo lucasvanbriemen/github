8113022841bb59177315218a9a1d6fa12db7f8bd
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-03-06 17:09:55 +0100

    Rename to Overview.

--- app/Mail/NotificationOverview.php
+++ app/Mail/NotificationOverview.php
@@ -29,7 +29,7 @@ class NotificationOverview extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'emails.notification_digest',
+            view: 'emails.notification_overview',
             with: [
                 'digestUrl' => url('/') . '/#/notifications/' . $this->date,
             ],
