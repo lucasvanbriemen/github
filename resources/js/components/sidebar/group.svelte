32107db429d73a0f9fc7584c8d9527bd68accf43
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-03-25 21:24:26 +0100

    Start open dialog.

--- resources/js/components/sidebar/group.svelte
+++ resources/js/components/sidebar/group.svelte
@@ -1,6 +1,8 @@
 <script>
+  import { onMount } from "svelte";
+
   let { title, children } = $props();
-  let isOpen = $state(false);
+  let isOpen = $state(true);
 
   let maxHeight = $state(0);
   let body;
@@ -15,6 +17,11 @@
       body.style.maxHeight = 0;
     }
   }
+
+  onMount(() => {
+    maxHeight = body.scrollHeight;
+    body.style.maxHeight = maxHeight + 'px';
+  });
 </script>
 
 <div class="group" class:open={isOpen}>
