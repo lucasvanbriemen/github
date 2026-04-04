492ebf2530c22421d03beb67d969ff564db83a4e
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-04-04 20:16:26 +0200

    Encode URL.

--- app/Helpers/Ably.php
+++ app/Helpers/Ably.php
@@ -11,7 +11,7 @@ class Ably
         $apiKey = config('services.ably.main_key');
 
         $response = Http::withBasicAuth(...explode(':', $apiKey, 2))
-            ->post("https://rest.ably.io/channels/{$channel}/messages", [
+            ->post("https://rest.ably.io/channels/".urlencode($channel)."/messages", [
                 'name' => $event,
                 'data' => is_string($data) ? $data : json_encode($data),
             ]);
