9267f14fe894c1677eda39288fa5ac70b2c3cc6c
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-04-04 19:44:41 +0200

    Refactor helper function for universal use.

--- app/Http/Controllers/AblyTestController.php
+++ app/Http/Controllers/AblyTestController.php
@@ -2,41 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\Ably;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\Http;
 
 class AblyTestController extends Controller
 {
     public function publishChannel1(): JsonResponse
     {
-        return $this->publish('channel-1', 'event', 'Hello from channel 1!');
+        $success = Ably::send('channel-1', 'Hello from channel 1!');
+
+        return response()->json(['success' => $success, 'channel' => 'channel-1']);
     }
 
     public function publishChannel2(): JsonResponse
     {
-        return $this->publish('channel-2', 'event', 'Hello from channel 2!');
-    }
+        $success = Ably::send('channel-2', 'Hello from channel 2!');
 
-    private function publish(string $channel, string $event, string $message): JsonResponse
-    {
-        $apiKey = config('services.ably.main_key');
-
-        $response = Http::withBasicAuth(...explode(':', $apiKey, 2))
-            ->post("https://rest.ably.io/channels/{$channel}/messages", [
-                'name' => $event,
-                'data' => $message,
-            ]);
-
-        if ($response->successful()) {
-            return response()->json([
-                'success' => true,
-                'channel' => $channel,
-            ]);
-        }
-
-        return response()->json([
-            'success' => false,
-            'error' => $response->body(),
-        ], $response->status());
+        return response()->json(['success' => $success, 'channel' => 'channel-2']);
     }
 }
