5440ca2a54320c2933c0ffa134fe89eb04394705
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-04-18 14:50:22 +0200

    Expose listing in API.

--- app/Http/Controllers/ReleaseController.php
+++ app/Http/Controllers/ReleaseController.php
@@ -2,16 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Release;
-use App\Services\RepositoryService;
-
 class ReleaseController extends Controller
 {
-    public function index($organizationName, $repositoryName)
-    {
-    }
+    public function index($organizationName, $repositoryName) {}
 
-    public function show(string $organizationName, string $repositoryName, int $projectNumber)
-    {
-    }
+    public function show(string $organizationName, string $repositoryName, int $projectNumber) {}
 }
