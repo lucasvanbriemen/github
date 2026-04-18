e3ba432828b89a433a75cb9762b5bd49162efdbb
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-04-18 14:51:52 +0200

    Return releases.

--- app/Http/Controllers/ReleaseController.php
+++ app/Http/Controllers/ReleaseController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\RepositoryService;
+
 class ReleaseController extends Controller
 {
-    public function index($organizationName, $repositoryName) {}
+    public function index($organizationName, $repositoryName)
+    {
+        [$organization, $repository] = RepositoryService::getRepositoryWithOrganization($organizationName, $repositoryName);
+        return $repository->releases()->orderBy('created_at', 'desc')->get();
+    }
 
-    public function show(string $organizationName, string $repositoryName, int $projectNumber) {}
+    public function show(string $organizationName, string $repositoryName, int $releaseNumber)
+    {
+        [$organization, $repository] = RepositoryService::getRepositoryWithOrganization($organizationName, $repositoryName);
+        return $repository->releases()->where('number', $releaseNumber)->firstOrFail();
+    }
 }
