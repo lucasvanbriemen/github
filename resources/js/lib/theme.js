9ba7bc4023b6dc77727f5c4b1299cf5373a54598
Lucas <contact@lucasvanbriemen.nl>
2026-05-08 13:06:15 +0200

    Merge custom and API colors to apply theme correctly.

--- resources/js/lib/theme.js
+++ resources/js/lib/theme.js
@@ -25,17 +25,14 @@ export default {
 
   async applyTheme() {
     document.documentElement.setAttribute("data-theme", this.getTheme());
-    const url = this.themeUrl.replace("THEME_NAME", this.getTheme());
-    const colors = await api.get(url);
+    const colors = await api.get(this.themeUrl);
 
-    colors.forEach(color => {
-      document.documentElement.style.setProperty(`--${color.name}`, color.value);
-    });
+    const mergedColors = { ...this.custom_colors, ...colors };
 
-    this.custom_colors.forEach(color => {
-      const name = `--${color.name}`;
+    Object.entries(mergedColors).forEach(([name, color]) => {
+      const cssVarName = `--${name}`;
       const value = this.getTheme() === "dark" ? color.dark : color.light;
-      document.documentElement.style.setProperty(name, value);
+      document.documentElement.style.setProperty(cssVarName, value);
     });
   },
 
