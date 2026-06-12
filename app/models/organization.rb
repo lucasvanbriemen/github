3e841e15759652c3c5ba527cefe9f3cd89e695ab
Lucas <contact@lucasvanbriemen.nl>
2026-06-12 21:36:32 +0200

    Style the text data.

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,3 +1,11 @@
 class Organization < ApplicationRecord
   has_many :repositories
+
+  def display_description
+    if description.present?
+      description
+    else
+      "No description provided."
+    end
+  end
 end
