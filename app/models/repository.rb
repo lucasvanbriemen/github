5f725dc05180b951f1b62855223fd641fc426874
Lucas <contact@lucasvanbriemen.nl>
2026-06-13 10:38:51 +0200

    Add display value.

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,3 +1,11 @@
 class Repository < ApplicationRecord
   belongs_to :organization
+
+  def display_description
+    if description.present?
+      description
+    else
+      "No description provided."
+    end
+  end
 end
