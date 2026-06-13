aba172ccd81dccfb3a4683cbf000cea0611bf701
Lucas <contact@lucasvanbriemen.nl>
2026-06-13 18:09:52 +0200

    Rename type column to kind and remove inheritance_column override.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ApplicationRecord
   self.inheritance_column = nil
   belongs_to :repository
+
+  # TODO: rename the type column to kind and remove this method and inheritance_column override
+  def kind
+    type
+  end
 end
