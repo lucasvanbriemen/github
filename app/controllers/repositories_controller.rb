bcca050a44ed87173dc35320b8fad83f1a84fbf7
Lucas <contact@lucasvanbriemen.nl>
2026-06-14 19:08:11 +0200

    Add all filter option if filter parameter is not provided.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -2,6 +2,6 @@ class RepositoriesController < ApplicationController
   def show
     organization = Organization.find_by!(name: params[:organization_name])
     @repository = organization.repositories.find_by!(name: params[:repository_name])
-    @items = @repository.items.public_send(params[:filter]).page(params[:page])
+    @items = @repository.items.public_send((params[:filter] || "all")).page(params[:page])
   end
 end
