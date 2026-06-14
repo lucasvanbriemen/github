2b9ca59cc2c0a257255fd216fb196171587df63c
Lucas <contact@lucasvanbriemen.nl>
2026-06-14 14:52:16 +0200

    Use pagination.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -2,5 +2,6 @@ class RepositoriesController < ApplicationController
   def show
     organization = Organization.find_by!(name: params[:organization_name])
     @repository = organization.repositories.find_by!(name: params[:repository_name])
+    @items = @repository.items.page(params[:page])
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   self.inheritance_column = nil
   belongs_to :repository
+  paginates_per 50
 
   # TODO: rename the type column to kind and remove this method and inheritance_column override
   def kind
