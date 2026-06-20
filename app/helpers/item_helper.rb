20e00623d46554216c9a28c4a0403499c30a8fff
Lucas <contact@lucasvanbriemen.nl>
2026-06-20 12:03:09 +0200

    Render SVG for item.

--- app/helpers/item_helper.rb
+++ app/helpers/item_helper.rb
@@ -6,4 +6,23 @@ module ItemHelper
       { label: "Pull requests", active: (params[:type] == "pull_requests"), href: items_path(organization_name: @repository.organization.name, repository_name: @repository.name, type: "pull_requests") }
     ]
   end
+
+  def item_svg(item)
+    svg_name = item.kind == "pull_request" ? "pull_request" : "issue"
+    inline_svg(svg_name, css_class: "item-icon item-#{item.kind} icon-#{item.state}", aria_label: item.kind)
+  end
+
+  private
+
+  def inline_svg(name, css_class: nil, aria_label: nil)
+    asset = Rails.application.assets.load_path.find("#{name}.svg")
+    raise Propshaft::MissingAssetError, "The asset '#{name}.svg' was not found in the load path." unless asset
+
+    attributes = []
+    attributes << %(class="#{css_class}") if css_class.present?
+    attributes << %(role="img" aria-label="#{aria_label}") if aria_label.present?
+
+    svg = asset.content.sub(/<svg\b/, [ "<svg", *attributes ].join(" "))
+    svg.html_safe
+  end
 end
