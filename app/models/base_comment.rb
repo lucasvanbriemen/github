b3652c16351cd91ad6508787ce84d67a0a40c121
Lucas <contact@lucasvanbriemen.nl>
2026-06-27 12:51:36 +0200

    Fix nil values in default_scope for BaseComment.

--- app/models/base_comment.rb
+++ app/models/base_comment.rb
@@ -1,6 +1,7 @@
 class BaseComment < ApplicationRecord
   self.inheritance_column = nil
   has_one :github_user, primary_key: :user_id, foreign_key: :id
+  default_scope { where.not(body: [ nil, "" ]) }
 
   belongs_to :item
 
