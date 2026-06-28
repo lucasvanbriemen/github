7c1ed8fbffa801a4e28cf34ca7bd37e22748053e
Lucas <contact@lucasvanbriemen.nl>
2026-06-28 22:16:03 +0200

    Limit private repositories.

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -2,6 +2,6 @@ class OrganizationsController < ApplicationController
   def index
     return forbidden unless can?(:read, :github, :repositories)
 
-    @organizations = Organization.all
+    @organizations = Organization.all.select { |organization| organization.visible_repositories(include_private: can?(:read, :github, :private_repositories)).any? }
   end
 end
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,5 +1,10 @@
 class Organization < ApplicationRecord
   has_many :repositories
+  has_many :public_repositories, -> { where(private: false) }, class_name: "Repository"
+
+  def visible_repositories(include_private:)
+    include_private ? repositories : public_repositories
+  end
 
   def slug
     name.downcase.gsub(" ", "-")
