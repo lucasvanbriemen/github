d9e323655ecd946ba0d026fc8527511b90a4af26
Lucas <contact@lucasvanbriemen.nl>
2026-07-12 13:48:47 +0200

    Use subscriber class for error reporting; Rails 8 requires #report.

--- config/initializers/ltvb_error_reporting.rb
+++ config/initializers/ltvb_error_reporting.rb
@@ -1,20 +1,24 @@
 require "net/http"
 
 if Rails.env.production? && ENV["LTVB_EXCEPTIONS_TOKEN"].present?
-  Rails.error.subscribe(->(error, handled:, severity:, context:, source: nil) do
-    Thread.new do
-      Net::HTTP.post(
-        URI("https://apps.ltvb.nl/api/exceptions"),
-        { token: ENV["LTVB_EXCEPTIONS_TOKEN"],
-          class: error.class.name,
-          message: error.message,
-          backtrace: error.backtrace&.first(50),
-          handled: handled, severity: severity,
-          context: context.transform_values(&:to_s),
-          occurred_at: Time.now.utc.iso8601 }.to_json,
-        "Content-Type" => "application/json")
-    rescue StandardError
-      # never take the app down over error reporting
+  class LtvbErrorSubscriber
+    def report(error, handled:, severity:, context:, source: nil)
+      Thread.new do
+        Net::HTTP.post(
+          URI("https://apps.ltvb.nl/api/exceptions"),
+          { token: ENV["LTVB_EXCEPTIONS_TOKEN"],
+            class: error.class.name,
+            message: error.message,
+            backtrace: error.backtrace&.first(50),
+            handled: handled, severity: severity,
+            context: context.transform_values(&:to_s),
+            occurred_at: Time.now.utc.iso8601 }.to_json,
+          "Content-Type" => "application/json")
+      rescue StandardError
+        # never take the app down over error reporting
+      end
     end
-  end)
+  end
+
+  Rails.error.subscribe(LtvbErrorSubscriber.new)
 end
